04ba0a4b6c2fc2bd71cd717046415299e498d38e
LongS7 <51916319+LongS7@users.noreply.github.com>
2021-03-17 08:56:54 +0700

    first commit

---
+++ app/src/main/java/com/longs7/lab1/LoginActivity02.java
@@ -0,0 +1,14 @@
+package com.longs7.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login02);
+    }
+}
---
+++ app/src/main/java/com/longs7/lab1/LoginActivity05.java
@@ -0,0 +1,14 @@
+package com.longs7.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity05 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login05);
+    }
+}
